27e64126b21fbcccccd8383f07dcce72c364bb3c
robin-jain <37232578+robin-jain@users.noreply.github.com>
2024-01-13 17:35:36 +0530

    Update TS_01_Login with the registered user.py
     this is change from user A'

--- test_scripts/TS_01_Login with the registered user.py
+++ test_scripts/TS_01_Login with the registered user.py
@@ -3,17 +3,9 @@ from libraries.generate_logs import logging
 
 logging.debug('This message is skipped as a level is set as INFO')
 logging.info('So should this')
-logging.info(' this is change from user')
+logging.info(' this is change from user A')
+ this is change from user A'
+ this is change from user A'
+ this is change from user A'
 logging.warning('And this, too')
 logging.error('Testing non-ASCII character, Ø and ö')
-
-
-#Donelogging.info('So should this')
-logging.info('So should this')
-logging.info('So should this')
-logging.info('So should this')
-
-
-logging.warning('And this, too')
-
-#done
