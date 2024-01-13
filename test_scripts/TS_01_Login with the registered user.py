938be4604094f3defaa042da93337f5365878953
robin <robin.jainwit@gmail.com>
2024-01-13 17:14:20 +0530

    Changes from user B

--- test_scripts/TS_01_Login with the registered user.py
+++ test_scripts/TS_01_Login with the registered user.py
@@ -4,7 +4,10 @@ from libraries.generate_logs import logging
 logging.debug('This message is skipped as a level is set as INFO')
 logging.info('So should this')
 logging.warning('And this, too')
-logging.warning('And this, too')
 logging.error('Testing non-ASCII character, Ø and ö')
 
-logging.warning('And this, too')
+
+#Donelogging.info('So should this')
+logging.info('So should this')
+logging.info('So should this')logging.info('So should this')
+
