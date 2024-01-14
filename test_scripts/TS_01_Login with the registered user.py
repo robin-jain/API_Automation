8e016c7b202483b6c2a24671f741256c04a9df35
robin-jain <37232578+robin-jain@users.noreply.github.com>
2024-01-14 13:34:25 +0530

    Update TS_01_Login with the registered user.py
    sas

--- test_scripts/TS_01_Login with the registered user.py
+++ test_scripts/TS_01_Login with the registered user.py
@@ -6,28 +6,6 @@ logging.info('So should this')
 logging.info(' this is change from user')
 logging.info(' this is change from user Final')
 logging.info(' this is change from user Final')
-logging.info('This commit from Siddarth')
-logging.info(' this is change from user from git hub ')
-logging.info(' this is change from user from git hub ')
-logging.info(' this is change from user git hub')
-logging.info(' this is change from user git hub')
-logging.info('This commit from git hub')
-logging.info(' this is change from user git hub')
-logging.info(' this is change from user git hub')
-
-logging.warning('And this, too')
-logging.error('Testing non-ASCII character, Ø and ö')
-
-logging.info(' this is change from user from git hub ')
-logging.info('So should Final')
-logging.info('So should Final')
-logging.info('This commit from git hub')
-logging.info('This commit from git hub')
-logging.info('This commit from git hub')
-logging.info('This commit from from git hub ')
-logging.info('This commit from from git hub ')
-logging.info('This commit from from git hub ')
-#test
 
 
 logging.warning('And this, too')
