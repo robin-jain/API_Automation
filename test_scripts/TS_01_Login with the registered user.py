18d989861c5bc5e150157c68d520da32daa49af9
robin-jain <37232578+robin-jain@users.noreply.github.com>
2024-01-14 13:35:50 +0530

    Update TS_01_Login with the registered user.py
    master

--- test_scripts/TS_01_Login with the registered user.py
+++ test_scripts/TS_01_Login with the registered user.py
@@ -3,22 +3,10 @@ from libraries.generate_logs import logging
 
 logging.debug('This message is skipped as a level is set as INFO')
 logging.info('So should this')
-logging.info(' this is change from user')
-logging.info(' this is change from user git hub')
-logging.info(' this is change from user git hub')
-logging.info('This commit from git hub')
-logging.info(' this is change from user git hub')
-logging.info(' this is change from user git hub')
-
-logging.warning('And this, too')
-logging.error('Testing non-ASCII character, Ø and ö')
-
-logging.info(' this is change from user B')
-logging.info('So should Final')
-logging.info('So should Final')
-logging.info('This commit from git hub')
-logging.info('This commit from git hub')
-logging.info('This commit from git hub')
+logging.info(' this is change from Master')
+logging.info(' this is change from Master')
+logging.info(' this is change from Master')
+logging.info(' this is change from Master')
 
 
 
