ef5bc971dfdc2e89c3bda045f0fcbbc5ac4f0a60
robin <robin.jainwit@gmail.com>
2024-01-25 20:00:13 +0530

    new .

--- test_scripts/TS_01_Login with the registered user.py
+++ test_scripts/TS_01_Login with the registered user.py
@@ -3,12 +3,16 @@ from libraries.generate_logs import logging
 
 logging.debug('This message is skipped as a level is set as INFO')
 logging.info('So should this')
-logging.info(' this is change from Master')
-logging.info(' this is change from Master')
-logging.info(' this is change from Master')
-logging.info(' this is change from Master')
-
-
+logging.info(' this is change from user Local')
+logging.info(' this is change from user')
+logging.info(' this is change from user Final')
+logging.info(' this is change from user Final')
+logging.info(' this is change from user Local')
 
 logging.warning('And this, too')
+logging.warning('And this, too')
+logging.warning('And this, too')
+logging.warning('And this, too')
+
+
 
